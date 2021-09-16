9b9f3ccd2a2ca352c94abbc233f52b926ea78809
alsansan <alberto_s.san@hotmail.com>
2021-09-16 18:49:04 +0200

    Datos de river estadio, resultado y promedios

---
+++ src/Mod2_Ejercicio3.java
@@ -0,0 +1,23 @@
+
+public class Mod2_Ejercicio3 {
+	public static void main(String[] args) {
+		System.out.println("Tipo de división\ta");
+		//Goles por partido
+		byte goleslocal = 3;
+		byte golesvisitante = 1;
+		System.out.println("Resultado\t"+ goleslocal + "-" + golesvisitante);
+		//Capacidad de la cancha
+		int cap = 70000;
+		System.out.println("Capacidad\t" + cap);
+		//Promedio de goles
+		short totalgoles=130;
+		byte totalpartidos=38;
+		double promedio=totalgoles*1.0/totalpartidos;
+		System.out.println("Promedio de Goles\t" + promedio);
+	
+		
+		
+	}
+
+
+}
