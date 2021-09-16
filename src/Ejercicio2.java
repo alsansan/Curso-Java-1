83a1a9845e84719ddcffbbac0cdb609d031c4c7e
alsansan <alberto_s.san@hotmail.com>
2021-09-16 17:37:15 +0200

    Initial commit

---
+++ src/Ejercicio2.java
@@ -0,0 +1,12 @@
+
+public class Ejercicio2 {
+	public static void main(String[] args) {
+		System.out.println("Nombre\t\tApellidos\t\temail\t\t\t\t\tteléfono");
+		System.out.println("------\t\t---------\t\t-----\t\t\t\t\t--------");
+		System.out.println("");
+		System.out.println("Alberto\t\tSánchez Sánchez\t\talberto_s.san@hotmail.com\t\t645352312");
+		
+	}
+
+
+}
