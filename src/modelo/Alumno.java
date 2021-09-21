693c8f34955846abdf344785e0dc178e8d6e1c8c
alsansan <alberto_s.san@hotmail.com>
2021-09-21 02:32:23 +0200

    clase alumno

--- src/modelo/Alumno.java
+++ src/modelo/Alumno.java
@@ -12,6 +12,23 @@ public class Alumno extends persona{
 		super(pnombre, papellido);
 		legajo=plegajo;
 	}
+	public int getLegajo() {
+		return legajo;
+	}
+	public void setLegajo(int legajo) {
+		this.legajo=legajo;
+	}
+	public boolean equals(Object obj){		
+		return obj instanceof Alumno	 					&&
+				super.equals(obj)  							&&
+				legajo == ((Alumno)obj).getLegajo();
+	}
+	public String toString() {
+		return super.toString() + ",legajo" + legajo ;
+	}		
+	public int hashCode(){
+		return super.hashCode() + (int)legajo;
+	}
 	
 	
 
