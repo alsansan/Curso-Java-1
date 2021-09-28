35d7caad1fd198839dab329acb03b66a0d983179
alsansan <alberto_s.san@hotmail.com>
2021-09-28 19:36:15 +0200

    practica 12

---
+++ WorkSpaceSpring/HolaSpringFrameWork/src/main/java/com/domain/modelo/dao/test/PracticasEliminar.sql
@@ -0,0 +1,5 @@
+DELETE FROM practicas where prac_nombre ='practica 1 - pantallas'
+DELETE FROM practicas where prac_nombre ='practica 2 - variables'
+DELETE FROM practicas where prac_nombre ='practica 3 - if-condiciones'
+-- ademas tengo que eliminar que quse se agrego con el metodo agregar
+DELETE FROM practicas where prac_nombre ='Gabriel_test'
---
+++ WorkSpaceSpring/HolaSpringFrameWork/target/classes/com/domain/modelo/dao/test/PracticasCrear.sql
@@ -0,0 +1,7 @@
+-- Eliminar
+INSERT INTO  practicas  (PRAC_NOMBRE ) VALUES ('practica 1 - pantallas')
+-- Modificar
+INSERT INTO  practicas  (PRAC_NOMBRE ) VALUES ('practica 2 - variables')
+-- Leer
+INSERT INTO  practicas  (PRAC_NOMBRE ) VALUES ('practica 3 - if-condiciones')
+INSERT INTO  practicas  (PRAC_NOMBRE ) VALUES ('practica 4 - while')
