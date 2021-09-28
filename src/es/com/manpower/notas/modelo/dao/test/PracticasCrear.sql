1d124897e90898a6a99f766dcf940b9aa1bc10fd
alsansan <alberto_s.san@hotmail.com>
2021-09-28 17:15:21 +0200

    pequeñas modificaciones e inicio practica 12

---
+++ src/es/com/manpower/notas/modelo/dao/test/PracticasCrear.sql
@@ -0,0 +1,7 @@
+-- Eliminar
+INSERT INTO  practicas  (PRAC_NOMBRE , ALU_APELLIDO ) VALUES ('Monsef_test','Bakhtaoui Dahraoui_test','FP2  Desarrollo De Aplicaciones Multiplataforma_test',' https://github.com/JMonsef/JavaCourse.git_test')
+-- Modificar
+INSERT INTO  practicas  (ALU_NOMBRE , ALU_APELLIDO ) VALUES ('Marina_test','Calvo Pereña_test','Grado en Física_test','https://gitlab.com/marinacalvo/cursojava2021.git_test')
+-- Leer
+INSERT INTO  practicas  (ALU_NOMBRE , ALU_APELLIDO ) VALUES ('Aarón_test','Sánchez Sánchez_test','Desarrollo de Aplicaciones Multiplataforma_test','https://github.com/Pashinian/CursoJava2021.git_test')
+INSERT INTO  practicas  (ALU_NOMBRE , ALU_APELLIDO ) VALUES ('Iván_test','Altares Palacios_test','Desarrollo de Aplicaciones Multiplataforma_test','https://gitlab.com/ivanaltares/ivanaltaresproyecto.git (fetch)_test')
